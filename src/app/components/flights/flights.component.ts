a29a3956e61d38050b3cf2035939a0375d35a68c
Armando Peña Tamayo (Ender) <ender@aleph.engineering>
2023-01-12 20:53:39 -0500

    Adding pagination system
    Finishing task number 2

--- src/app/components/flights/flights.component.ts
+++ src/app/components/flights/flights.component.ts
@@ -8,16 +8,61 @@ import { Flight, PaginateFlights } from 'src/app/models';
   styleUrls: ['./flights.component.scss']
 })
 export class FlightsComponent {
+  private _page: number = 1;
+  private _perPage: number = 10;
   flights: Flight[] = [];
-  page: number = 1;
-  perPage: number = 10;
+  totalFlights: number = 0;
+  totalPages: number = 1;
 
   constructor(private _flightsService: FlightService) { }
 
   ngOnInit() {
+    this.loadFlights();
+  }
+
+  private loadFlights() {
     this._flightsService.getFlightsPage(this.page, this.perPage).then((data: PaginateFlights) => {
       this.flights = data.items;
+      this.totalFlights = data.total;
+      this.totalPages = data.totalPages;
       console.log(this.flights);
     });
+  }
+
+  private isValidPageChange(change: number = 1): boolean {
+    const page = this._page + change;
+    return (page > 0 && page <= this.totalPages);
+  }
+
+  set page(change: number) {
+    if (!this.isValidPageChange(change)) return;
+    this._page += change;
+    console.log({ change, page: this._page });
+    this.loadFlights();
+  }
+
+  get page(): number { return this._page; }
+
+  set perPage(change: number) {
+    this._perPage = change;
+    this.loadFlights();
+  }
+
+  get perPage(): number { return this._perPage; }
+
+  prevPag(): void {
+    this.page = -1;
+  }
+
+  nextPag(): void {
+    this.page = 1;
+  }
+
+  range(i: number): number[] {
+    return Array(i).fill(0).map((x, i) => i + 1);
+  }
+
+  changeItemsPerPage(change: string): void {
+    this.perPage = parseInt(change);
   }
 }
