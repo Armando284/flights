f353d9fc970afc5d04e337813cf4c109c1fff7ef
Armando Peña Tamayo (Ender) <ender@aleph.engineering>
2023-01-14 18:42:47 -0500

    Setting the routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FlightsComponent } from './components/flights/flights.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'flights/page/:page/perPage/:perPage', component: FlightsComponent },
+  { path: '', redirectTo: 'flights/page/1/perPage/10', pathMatch: 'full' },
+  { path: '**', component: NotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
