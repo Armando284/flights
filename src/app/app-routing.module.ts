6cbef38cfd2c4e6c8d72a772e7f640f4de9e2e76
Armando Peña Tamayo (Ender) <ender@aleph.engineering>
2023-01-15 15:50:52 -0500

    Adding create flights route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { FlightsComponent } from './components/flights/flights.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
+import { CreateFlightComponent } from './components/create-flight/create-flight.component';
 
 const routes: Routes = [
   { path: 'flights/page/:page/perPage/:perPage', component: FlightsComponent },
   { path: '', redirectTo: 'flights/page/1/perPage/10', pathMatch: 'full' },
+  { path: 'flights/create', component: CreateFlightComponent },
   { path: '**', component: NotFoundComponent }
 ];
 
